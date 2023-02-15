a4ae80e3572956e10088a587cbe3ac1523e3ac22
OleksiyMunenko <oleksiymun@gmail.com>
2023-02-15 12:54:48 +0200

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ };
+
+ const startBtn = document.querySelector('button[data-start]');
+ const stopBtn = document.querySelector('button[data-stop]');
+ const body = document.querySelector('body');
+
+
+ startBtn.addEventListener('click', randomBgColorStart);
+ stopBtn.addEventListener('click', stoprandomBgColor);
+
+ stopBtn.disabled = true;
+
+ function randomBgColorStart(){
+	startBtn.disabled = true;
+	stopBtn.disabled = false;
+
+	timerId = setInterval(() =>	{
+		body.style.backgroundColor = getRandomHexColor();
+	}, 1000)
+ };
+
+
+ function stoprandomBgColor() {
+	startBtn.disabled = false;
+	stopBtn.disabled = true;
+
+	clearInterval(timerId);
+ }
+
+
+
 
